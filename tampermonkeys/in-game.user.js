9d821edff573cf44d5640bd292ae719aa235b30c
Zashness <zashness@gmail.com>
2025-07-17 20:03:22 -0400

    Update tampermonkey for pre-game and post-game animation and background

--- tampermonkeys/in-game.user.js
+++ tampermonkeys/in-game.user.js
@@ -15,7 +15,23 @@
   // Inject a CSS rule that matches any class beginning with that prefix
   GM_addStyle(`
     @import url('https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Oswald:wght@200..700&display=swap');
-    /* ==========In-game/Duels mode =========== */
+
+    [class*="game_backgroundDefault__"][class*="game_backgroundDefault__"] {
+      --background: url("https://rashinban.org/assets/images/key-visual.png");
+    }
+    /* In-game background & post-game background */
+    [class^="views_activeRoundWrapper__"][class^="views_activeRoundWrapper__"],
+    [class^="overlay_backdrop__"][class^="overlay_backdrop__"] {
+      background: var(--background);
+      background-repeat: no-repeat !important;
+      background-position: center center !important;
+      background-size: cover !important;
+    }
+
+    /* ===================================== */
+    /* ======== In-game/Duels mode ========= */
+    /* ===================================== */
+
     /* The main shaded notch on top of the screen */
     [class^="game-notch_shield__"] {
       display: none;
@@ -102,38 +118,63 @@
       top: -18px;
     }
 
-    /* ==========Game preview mode=========== */
+
+    /* ===================================== */
+    /* ========= Game preview mode ========= */
+    /* ===================================== */
+
     [class^="cam-hud_wrapper__"]:has(~ [class^="preview-round_guessMapContainer__"]) {
       visibility:hidden;
     }
 
-    /* ===========Post game mode============ */
+
+
+    /* ===================================== */
+    /* ========== Post game mode =========== */
+    /* ===================================== */
+
+    /* Prevent HP from moving during post game */
     [class*="cam-hud_wrapper__"][class*="cam-hud_wrapper__"][class*="cam-hud_inRoundResult"] {
       transform: none;
     }
+    /* Change position of the scores. Move up and spread to edge of screen. */
     [class*="round-score-animations_scoreTable"][class*="round-score-animations_scoreTable"][class*="round-score-animations_scoreTable"] {
       width: 1820px;
-      /*top: -256px;*/
-      /*position: relative;*/
       margin-top: -392px;
     }
+    /* Fonts for the distance and scores */
     [class*="round-score-animations_scoreContainer__"][class*="round-score-animations_scoreContainer__"],
     [class*="round-score-animations_static__"][class*="round-score-animations_static__"],
     [class*="round-score-animations_distanceRow__"][class*="round-score-animations_distanceRow__"],
-    [class*="round-score-animations_scoreColumnLeft"][class*="round-score-animations_scoreColumnLeft"],
-    [calss*="round-score-animations_scoreColumnRight"][calss*="round-score-animations_scoreColumnRight"] {
+    [class*="round-score-animations_scoreColumn"][class*="round-score-animations_scoreColumn"] {
       font-family: "Barlow", sans-serif;
       font-weight: 500;
       font-style: normal;
     }
+    /* Not sure why this can't be combined with above...but needed here separately */
     [class*="round-score-map_mapContainer__"][class*="round-score-map_mapContainer__"] {
       top: -200px;
       position: relative;
       height: 502px;
     }
+    /* Remove divider between distance and score */
     [class*="round-score-animations_divider__"][class*="round-score-animations_divider__"],
     [class*="round-score-animations_scoreTitle__"][class*="round-score-animations_scoreTitle__"] {
       visibility: hidden;
+    }
+    /* Remove blur and color effect on score */
+    [class^="shadow-text_root__"][class^="shadow-text_root__"] {
+      text-shadow: none;
+      font-size: 96px;
+      color: white;
+    }
+    [class*="round-score-animations_static__"][class*="round-score-animations_static__"] {
+      /*opacity: 0.8;*/
+    }
+
+    /* Distance */
+    [class^="round-score-animations_scoreColumn"][class^="round-score-animations_scoreColumn"] {
+      font-size: 24px;
     }
 `);
 })();
