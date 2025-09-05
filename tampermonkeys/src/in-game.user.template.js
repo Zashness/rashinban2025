1ff9fba066307d6e6e072455b61c48faefbfa5e8
Zashness <zashness@gmail.com>
2025-09-05 18:30:33 -0400

    Allow mouse clicks through player name/score topbar

--- tampermonkeys/src/in-game.user.template.js
+++ tampermonkeys/src/in-game.user.template.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rashinban Geoguessr Game Master Mode
 // @namespace    https://github.com/Zashness/rashinban2025/blob/gh-pages/tampermonkeys/out/in-game.user.js
-// @version      1.1.0
+// @version      1.1.1
 // @description  Game Master Mode mods for Rashinban2025
 // @author       Zashness
 // @match        https://www.geoguessr.com/*
