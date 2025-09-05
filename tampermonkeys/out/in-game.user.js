5b365d3ec721c80cc87c68e5015fdb103a12f7c2
Zashness <zashness@gmail.com>
2025-09-05 18:49:35 -0400

    Game Master: move "Disband Party" to below player name/score.

--- tampermonkeys/out/in-game.user.js
+++ tampermonkeys/out/in-game.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rashinban Geoguessr Game Master Mode
 // @namespace    https://github.com/Zashness/rashinban2025/blob/gh-pages/tampermonkeys/out/in-game.user.js
-// @version      1.1.2
+// @version      1.1.3
 // @description  Game Master Mode mods for Rashinban2025
 // @author       Zashness
 // @match        https://www.geoguessr.com/*
@@ -306,6 +306,14 @@ window.addEventListener('load', function () {
     /* Distance */
     [class^="round-score-animations_scoreColumn"][class^="round-score-animations_scoreColumn"] {
       font-size: 24px;
+    }
+
+    /* ===================================== */
+    /* ====== Game Master Setup Mode ======= */
+    /* ===================================== */
+    [data-qa="disband-party-button"] {
+      top: 160px;
+      z-index: 3;
     }
 `);
 
