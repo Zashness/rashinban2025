a9fb5502a3c43709c55366e07374fd449b3cca4e
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2025-10-25 09:32:03 +0900

    move sponsor in in-game up a bit

--- tampermonkeys/src/in-game.user.template.js
+++ tampermonkeys/src/in-game.user.template.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Rashinban Geoguessr Game Master Mode
 // @namespace    https://github.com/Zashness/rashinban2025/blob/gh-pages/tampermonkeys/out/in-game.user.js
-// @version      1.1.13
+// @version      1.1.14
 // @description  Game Master Mode mods for Rashinban2025
 // @author       Zashness
 // @match        https://www.geoguessr.com/*
@@ -398,7 +398,7 @@ window.addEventListener('load', function () {
     containerId: 'sponsors',
     leftLogos: [],
     rightLogos: logos,
-    bottom: 135,
+    bottom: 155,
     height: 84,
     gap: 0,
   });
